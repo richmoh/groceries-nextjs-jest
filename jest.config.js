fe1830685e1091f3287879e5f0fb29475e0e791e
Richard Mohammed <richard@oneabsolute.co.uk>
2024-08-13 17:32:13 +0100

    Initial setup - jest and prisma

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 const nextJest = require("next/jest");
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
+  dir: "./"
 });
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testEnvironment: "jsdom",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" })
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
